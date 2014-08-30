5a47034f1357d16ed006b599bf204d3aa4ee3976
marcelkornblum <me@marcelkornblum.com>
2014-08-30 16:00:10 +0100

    search

--- client/brixtondd/partial/search/search.js
+++ client/brixtondd/partial/search/search.js
@@ -1,5 +1,18 @@
 angular.module('brixtondd')
-    .controller('SearchCtrl',function($scope){
+    .controller('SearchCtrl',function($scope, $q, events, venues, artists, artworks, zones){
 
+        $scope.searchText = '';
 
+        $q.all([events.getList(),
+               venues.getList(),
+               artists.getList(),
+               artworks.getList(),
+               zones.getList()])
+            .then(function(everything) {
+                $scope.events = everything[0];
+                $scope.venues = everything[1];
+                $scope.artists = everything[2];
+                $scope.artworks = everything[3];
+                $scope.zones = everything[4];
+            });
     });
